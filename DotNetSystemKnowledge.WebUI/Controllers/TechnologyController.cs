ae0c293bbc26fada963a83ccb79048db5515676b
Alena Tarasiuk <Alenka_Tarsuk14@mail.ru>
2014-11-11 11:34:06 +0300

    add styles and header

--- DotNetSystemKnowledge.WebUI/Controllers/TechnologyController.cs
+++ DotNetSystemKnowledge.WebUI/Controllers/TechnologyController.cs
@@ -5,20 +5,35 @@ using System.Web;
 using System.Web.Mvc;
 using DotNetSystemKnowledge.Domain.Entities;
 using DotNetSystemKnowledge.Domain.Interfaces;
+using DotNetSystemKnowledge.WebUI.Models;
 
 namespace DotNetSystemKnowledge.WebUI.Controllers
 {
     public class TechnologyController : Controller
     {
         private ITechnologyRepository repository;
+        public int PageSize = 5;
         public TechnologyController(ITechnologyRepository technologyRepository)
         {
             this.repository = technologyRepository;
         }
 
-        public ViewResult List()
+        public ViewResult List(int page = 1)
         {
-            return View(repository.Technologys);
+            TechnologysViewModel model = new TechnologysViewModel
+            {
+                Technologys = repository.Technologys
+                .OrderBy(p => p.TechnologyID)
+                .Skip((page - 1) * PageSize)
+                .Take(PageSize),
+                PagingInfo = new PagingInfo
+                {
+                    CurrentPage = page,
+                    ItemsPerPage = PageSize,
+                    TotalItems = repository.Technologys.Count()
+                }
+            };
+            return View(model);
         }
     }
 }
---
+++ DotNetSystemKnowledge.WebUI/Models/TechnologysViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using DotNetSystemKnowledge.Domain.Entities;
+
+namespace DotNetSystemKnowledge.WebUI.Models
+{
+    public class TechnologysViewModel
+    {
+        public IEnumerable<Technology> Technologys { get; set; }
+        public PagingInfo PagingInfo { get; set; }
+    }
+}
